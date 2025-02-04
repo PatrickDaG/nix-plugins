1ae8d0c7faa187c388bdebc54ed05c9722d26f11
Patrick <patrick@failmail.dev>
2025-02-04 15:22:44 +0100

    feat: switch to lix

--- extra-builtins.cc
+++ extra-builtins.cc
@@ -1,8 +1,11 @@
-#include <config.h>
-#include <primops.hh>
-#include <globals.hh>
-#include <eval-inline.hh>
-#include <eval-settings.hh>
+#include <lix/config.h>
+
+#include <lix/libexpr/primops.hh>
+#include <lix/libexpr/eval-inline.hh>
+#include <lix/libexpr/eval-settings.hh>
+
+#include <lix/libstore/globals.hh>
+
 #include <dlfcn.h>
 
 #include "nix-plugins-config.h"
@@ -31,34 +34,33 @@ static GlobalConfig::Register rp(&extraBuiltinsSettings);
 static void extraBuiltins(EvalState & state, const PosIdx pos,
     Value ** _args, Value & v)
 {
-    static auto extraBuiltinsFile = state.rootPath(CanonPath(extraBuiltinsSettings.extraBuiltinsFile.to_string()));
-    if (state.allowedPaths)
-        state.allowedPaths->insert(extraBuiltinsFile.path.abs());
+    static auto extraBuiltinsFile = SourcePath(CanonPath(extraBuiltinsSettings.extraBuiltinsFile.to_string()));
+	state.ctx.paths.allowPath(extraBuiltinsFile.canonical().abs());
 
     try {
-        auto fun = state.allocValue();
+        auto fun = state.ctx.mem.allocValue();
         state.evalFile(extraBuiltinsFile, *fun);
         Value * arg;
         if (evalSettings.enableNativeCode) {
-            arg = state.baseEnv.values[0];
+            arg = state.ctx.builtins.env.values[0];
         } else {
-            auto attrs = state.buildBindings(2);
+            auto attrs = state.ctx.buildBindings(2);
 
-            auto sExec = state.symbols.create("exec");
+            auto sExec = state.ctx.symbols.create("exec");
             attrs.alloc(sExec).mkPrimOp(new PrimOp {
                 .name = "exec",
                 .arity = 1,
                 .fun = prim_exec,
             });
 
-            auto sImportNative = state.symbols.create("importNative");
+            auto sImportNative = state.ctx.symbols.create("importNative");
             attrs.alloc(sImportNative).mkPrimOp(new PrimOp {
                 .name = "importNative",
                 .arity = 2,
                 .fun = prim_importNative,
             });
 
-            arg = state.allocValue();
+            arg = state.ctx.mem.allocValue();
             arg->mkAttrs(attrs);
         }
         v.mkApp(fun, arg);
@@ -79,7 +81,7 @@ static RegisterPrimOp rp1({
 static void cflags(EvalState & state, const PosIdx _pos,
     Value ** _args, Value & v)
 {
-    auto attrs = state.buildBindings(3);
+    auto attrs = state.ctx.buildBindings(3);
     attrs.alloc("NIX_INCLUDE_DIRS").mkString(NIX_INCLUDE_DIRS);
     attrs.alloc("NIX_CFLAGS_OTHER").mkString(NIX_CFLAGS_OTHER);
     attrs.alloc("BOOST_INCLUDE_DIR").mkString(BOOST_INCLUDE_DIR);
